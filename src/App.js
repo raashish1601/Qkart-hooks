18910a583dbc667f329c9293734174167dbc81e1
Raashish Aggarwal <94279692+raashish1601@users.noreply.github.com>
2022-12-05 21:56:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import theme from "./theme";
 import Thanks from "./components/Thanks";
 import Checkout from "./components/Checkout";
 export const config = {
-  endpoint: `https://raashish-qkart-backend.onrender.com`
+  endpoint: `https://raashish-qkart-backend.onrender.com/api/v1`,
 };
 
 function App() {
